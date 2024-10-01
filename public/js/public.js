a5e175d54fc0b781e2662eac2ad22aa3d5c326c8
Escala Digital <info@escaladigital.es>
2024-10-01 11:59:20 +0200

    propiedades en el geojson de la BD

--- public/js/public.js
+++ public/js/public.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     if ($('#nm-main-map').length) {
-     
+
         var map = L.map('nm-main-map').setView([nmMapData.lat, nmMapData.lng], nmMapData.zoom);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,10 +11,10 @@ jQuery(document).ready(function($) {
         $.post(nmMapData.ajax_url, {
             action: 'nm_get_map_points',
             nonce: nmMapData.nonce
-        }, function(response) {
+        }, function (response) {
             console.log(response);
             if (Array.isArray(response)) {
-                response.forEach(function(feature) {
+                response.forEach(function (feature) {
                     if (feature.geometry && feature.geometry.type === 'Point') {
                         var coords = feature.geometry.coordinates;
                         var lng = coords[0];
@@ -29,8 +29,8 @@ jQuery(document).ready(function($) {
             } else {
                 console.error('Invalid response from server:', response);
             }
-            
-        }).fail(function(jqXHR, textStatus, errorThrown) {
+
+        }).fail(function (jqXHR, textStatus, errorThrown) {
             console.error('AJAX Error:', textStatus, errorThrown);
         });
     }
@@ -58,41 +58,60 @@ jQuery(document).ready(function($) {
         });
 
         // Handle form submission
-        $('#nm-user-form').submit(function(e) {
+        $('#nm-user-form').submit(function (e) {
             e.preventDefault();
-        
+
             var formData = new FormData(this);
-        
+
             // Agregar el parámetro 'action' requerido por WordPress
             formData.append('action', 'nm_submit_form');
-        
+
             // Agregar el nonce para la verificación de seguridad
             formData.append('nonce', nmPublic.nonce);
-        
+
             // Obtener los datos de los campos dibujados en el mapa
             var shapes = [];
-            drawnItems.eachLayer(function(layer) {
-                shapes.push(layer.toGeoJSON());
+            var otherFields = $(this).serializeArray();
+
+            drawnItems.eachLayer(function (layer) {
+                var geoJson = layer.toGeoJSON();
+
+                // Crear un nuevo objeto GeoJSON con 'geometry' antes que 'properties'
+                var orderedGeoJson = {
+                    type: geoJson.type,
+                    geometry: geoJson.geometry,
+                    properties: {}
+                };
+
+                // Añadir otros campos personalizados al GeoJSON en el orden natural
+                for (var i = 0; i < otherFields.length; i++) {
+                    var field = otherFields[i];
+                    orderedGeoJson.properties['nm_' + field.name] = field.value;
+                }
+
+                shapes.push(orderedGeoJson);
             });
+
             formData.append('form_data[map_data]', JSON.stringify(shapes));
-        
-            // (Opcional) Obtener otros campos personalizados si no están incluidos automáticamente
-        
+
+
+
+
+            console.log(formData);
             $.ajax({
                 url: nmPublic.ajax_url,
                 method: 'POST',
                 data: formData,
                 processData: false,
                 contentType: false,
-                success: function(response) {
-                    alert('Form submitted successfully.');
+                success: function (response) {
+                    alert('Form submitted successfully.' + JSON.stringify(formData));
                 },
-                error: function(jqXHR, textStatus, errorThrown) {
+                error: function (jqXHR, textStatus, errorThrown) {
                     alert('Error submitting form: ' + textStatus);
                     console.error('AJAX Error:', textStatus, errorThrown);
                 }
             });
         });
-        
     }
 });
