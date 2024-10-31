10bd0f2b4937c972a4464b2c920690e210373d96
Escala Digital <info@escaladigital.es>
2024-10-31 12:46:31 +0100

    Rename gitignore method to save_form and update field template label from Date Picker to File

--- admin/views/field-templates/file.php
+++ admin/views/field-templates/file.php
@@ -1,5 +1,5 @@
-<div class="nm-form-field" data-type="date">
-    <label>Date Picker</label>
+<div class="nm-form-field" data-type="File">
+    <label>File</label>
     <input type="text" class="field-label" placeholder="Field Label" value="<?php echo esc_attr( $field_label ); ?>">
     <input type="text" class="field-name" placeholder="Field Name" value="<?php echo esc_attr( $field_name ); ?>">
     <span class="nm-remove-field">Remove Field</span>
